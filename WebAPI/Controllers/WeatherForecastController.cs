20a7621f3aa3be45a8dbcc5f936e6786f2db76ff
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2025-01-07 13:22:24 +0100

    lab 7

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -3,10 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
@@ -29,5 +29,48 @@ namespace WebAPI.Controllers
             })
             .ToArray();
         }
+
+        [HttpPost("Quot")]
+        public double Quot(int x, int y)
+        {
+            if (y <= 0) throw new ArgumentOutOfRangeException("Y must bi higher than 0");
+            return x / y;
+        }
+
+        [HttpPost("Prod")]
+        public double Prod(int x, int y)
+        {
+            return x * y;
+        }
+
+        [HttpGet]
+        public string GetSummary(int x)
+        {
+            return Summaries[x];
+        }
+
+        [HttpPost]
+        public string[] PostSummary(string item)
+        {
+            var items = Summaries.Append(item);
+            return items.ToArray();
+        }
+
+        [HttpDelete]
+        public string[] RemoveSummary(int x)
+        {
+            var items = Summaries.ToList();
+            items.RemoveAt(x);
+            return items.ToArray();
+        }
+
+        [HttpPut]
+        public string[] UpdateSummary(int x, string y)
+        {
+            var items = Summaries.ToList();
+            items[x] = y;
+            return items.ToArray();
+        }
+
     }
 }
--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1aff3f4909afc9879c320597420840b7ffa5780")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
